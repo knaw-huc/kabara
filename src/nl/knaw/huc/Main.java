fd050b3083cd82a7f72acc78af878f1b063f1f30
Meindert Kroese <Meindert.Kroese@huygens.knaw.nl>
2019-04-16 12:28:27 +0200

    Cleanup code

--- src/nl/knaw/huc/Main.java
+++ src/nl/knaw/huc/Main.java
@@ -23,25 +23,8 @@ public class Main {
 
   public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
 
-    // getProperties();
-
     System.out.println(readProperties());
 
-    // write your code here
-  }
-
-  private static void getProperties() throws ParserConfigurationException, IOException, SAXException {
-    File inputFile = new File("config.xml");
-    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-    Document doc = dBuilder.parse(inputFile);
-    doc.getDocumentElement().normalize();
-    System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-    NodeList nList = doc.getElementsByTagName("resourcesync");
-    for (int i = 0; i < nList.getLength(); i++) {
-      Node nNode = nList.item(i);
-      System.out.println("\nCurrent Element :" + nNode.getNodeName()+" - "+nNode.getTextContent());
-    }
   }
 
   private static Properties readProperties() throws IOException {
