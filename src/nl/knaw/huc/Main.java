087bf3da2d8cd8cc6c4ae7487dd6e64562e83986
Meindert Kroese <Meindert.Kroese@huygens.knaw.nl>
2019-04-16 12:20:02 +0200

    Reading a config file

--- src/nl/knaw/huc/Main.java
+++ src/nl/knaw/huc/Main.java
@@ -15,6 +15,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Date;
 import java.util.Properties;
 
@@ -22,7 +23,9 @@ public class Main {
 
   public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
 
-    getProperties();
+    // getProperties();
+
+    System.out.println(readProperties());
 
     // write your code here
   }
@@ -40,4 +43,15 @@ public class Main {
       System.out.println("\nCurrent Element :" + nNode.getNodeName()+" - "+nNode.getTextContent());
     }
   }
+
+  private static Properties readProperties() throws IOException {
+    Properties prop = new Properties();
+    ClassLoader loader = Thread.currentThread().getContextClassLoader();
+    InputStream stream = new FileInputStream("config.properties");
+    System.err.println(stream);
+    InputStreamReader isr = new InputStreamReader(stream);
+    prop.load(stream);
+    return prop;
+  }
+
 }
