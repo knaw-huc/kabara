9f1600693671772e35459e145bb86e2fc56abfd3
meindertkroese <>
2019-10-07 14:40:44 +0200

    prelimenarywork to make request more easy

--- src/main/java/nl/knaw/huc/di/kabara/resources/KabaraResource.java
+++ src/main/java/nl/knaw/huc/di/kabara/resources/KabaraResource.java
@@ -2,11 +2,15 @@ package nl.knaw.huc.di.kabara.resources;
 
 import com.codahale.metrics.annotation.Timed;
 import nl.knaw.huc.di.kabara.api.Saying;
+import nl.knaw.huc.di.kabara.api.SyncRequest;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.concurrent.atomic.AtomicLong;
 
 
@@ -25,5 +29,11 @@ public class KabaraResource {
   @Timed
   public Saying sayHello() {
     return new Saying(counter.incrementAndGet(), "Kabara active");
+  }
+
+  @POST
+  public Response syncDataSet(SyncRequest request) {
+    LoggerFactory.getLogger(KabaraResource.class).info("dataset, {}" , request.getDataSet());
+    return Response.ok().build();
   }
 }
