0deb8b26caa879bd9192a175324b039237f89cf6
Martijn Maas <martijn.maas@huygens.knaw.nl>
2020-02-28 08:28:13 +0100

    Add convenience script to debug

--- local_build_and_test.sh
+++ local_build_and_test.sh
@@ -4,12 +4,14 @@ mvn clean package
 
 set VIRTUOSO_HOST="http://localhost:8890"
 
-docker run --name my-virtuoso \
-    -p 8890:8890 -p 1111:1111 \
+docker run -p 8890:8890 -p 1111:1111 \
     -e DBA_PASSWORD=mysecret \
     -e SPARQL_UPDATE=true \
     -e DEFAULT_GRAPH=http://www.example.com/my-graph \
-    -v ./data/virtuoso:/data \
-    -d virtuoso:1.3.1-virtuoso7.2.2
+    -d tenforce/virtuoso:1.3.1-virtuoso7.2.2
+
+JAVA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
+
+export JAVA_OPTS
 
 ./target/appassembler/bin/kabara server kabara.yml
